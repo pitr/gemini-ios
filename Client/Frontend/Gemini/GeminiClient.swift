f86979034a2a8c29b36eb0b8c690ba24274dbd93
☃ pitr <pitr.vern@gmail.com>
2020-05-18 22:48:16 +0200

    fixes for gemini client

--- Client/Frontend/Gemini/GeminiClient.swift
+++ Client/Frontend/Gemini/GeminiClient.swift
@@ -5,7 +5,7 @@ private let log = Logger.browserLogger
 
 enum GeminiClientStatus {
     case input(String)
-    case success(String)
+    case success(String, String.Encoding)
     case redirect(String)
     case failure(String)
     case certificateRequired(String)
@@ -122,17 +122,54 @@ extension GeminiClient: StreamDelegate {
 
     // gemini://gemini.circumlunar.space/
     fileprivate func parseResponse(data: Data) {
-        guard let content = String(data: data, encoding: .utf8),
-            let ix = content.range(of: "\r\n")
+        guard let ix = data.firstIndex(of: 13),
+            data[ix+1] == 10,
+            let header = String(data: data.prefix(upTo: ix), encoding: .utf8)
             else {
                 urlSchemeTask.didFailWithError(GeminiClientError.responderUnableToHandle)
                 return
         }
-        let header = String(content[..<ix.lowerBound])
         log.debug("header: \(header)")
-        let status = parseStatus(header: header, content: content)
+        let status = parseStatus(header: header)
         switch status {
-        case .success(let body):
+        case .success(let mime, let encoding):
+            let data = data.dropFirst(ix+2)
+            if mime.starts(with: "text/") {
+                guard let body = String(data: data, encoding: encoding) else {
+                    urlSchemeTask.didFailWithError(GeminiClientError.responderUnableToHandle)
+                    return
+                }
+                if mime == "text/gemini" {
+                    guard let resp = parseBody(body).data(using: .utf8) else {
+                        urlSchemeTask.didFailWithError(GeminiClientError.responderUnableToHandle)
+                        return
+                    }
+                    urlSchemeTask.didReceive(URLResponse(url: url, mimeType: "text/html", expectedContentLength: -1, textEncodingName: "utf-8"))
+                    urlSchemeTask.didReceive(resp)
+                } else {
+                    guard let resp = body.data(using: .utf8) else {
+                        urlSchemeTask.didFailWithError(GeminiClientError.responderUnableToHandle)
+                        return
+                    }
+                    urlSchemeTask.didReceive(URLResponse(url: url, mimeType: "text/plain", expectedContentLength: -1, textEncodingName: "utf-8"))
+                    urlSchemeTask.didReceive(resp)
+                }
+            } else {
+                urlSchemeTask.didReceive(URLResponse(url: url, mimeType: mime, expectedContentLength: -1,textEncodingName: nil))
+                urlSchemeTask.didReceive(data)
+            }
+            urlSchemeTask.didFinish()
+        case .redirect(let to):
+            let body = "<meta http-equiv=\"refresh\" content=\"0; URL='\(to)'\" />"
+            guard let data = body.data(using: .utf8) else {
+                urlSchemeTask.didFailWithError(GeminiClientError.responderUnableToHandle)
+                return
+            }
+            urlSchemeTask.didReceive(URLResponse(url: url, mimeType: "text/html", expectedContentLength: -1, textEncodingName: "utf-8"))
+            urlSchemeTask.didReceive(data)
+            urlSchemeTask.didFinish()
+        case .input(let question):
+            let body = "<!DOCTYPE html><html lang=\"en\"><head><title>\(question)</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head><body><h2>\(question)</h2><form><textarea id=q name=q></textarea><hr /><button>Submit</button></form><script>document.forms[0].onsubmit=function(e){e.preventDefault();document.location=document.location.origin+document.location.pathname+\"?\"+encodeURI(document.getElementById(\"q\").value)}</script>"
             guard let data = body.data(using: .utf8) else {
                 urlSchemeTask.didFailWithError(GeminiClientError.responderUnableToHandle)
                 return
@@ -145,11 +182,45 @@ extension GeminiClient: StreamDelegate {
         }
     }
 
-    fileprivate func parseStatus(header: String, content: String) -> GeminiClientStatus {
+    fileprivate func parseStatus(header: String) -> GeminiClientStatus {
         switch header.prefix(1) {
-        case "1": return .input(header)
-        case "2": return .success(parseBody(content))
-        case "3": return .redirect(header)
+        case "1": return .input(String(header.dropFirst(2)))
+        case "2":
+            do {
+                let mimeRegex = try NSRegularExpression(pattern: #"^\d+\s+([^\s;]+)"#, options: [])
+                let charsetRegex = try NSRegularExpression(pattern: #"charset\s*=\s*([^\s;]+)"#, options: [])
+                let range = NSRange(header.startIndex..<header.endIndex, in: header)
+                guard let m1 = mimeRegex.firstMatch(in: header, options: [], range: range),
+                    let range1 = Range(m1.range(at: 1), in: header) else {
+                        return .failure("cannot parse header")
+                }
+                let mime = String(header[range1])
+                var charset = "utf-8"
+                if let m2 = charsetRegex.firstMatch(in: header, options: [], range: range),
+                    let range2 = Range(m2.range(at: 1), in: header) {
+                    charset = String(header[range2])
+                }
+                let cfEncoding = CFStringConvertIANACharSetNameToEncoding(charset as CFString)
+                let nsEncoding = CFStringConvertEncodingToNSStringEncoding(cfEncoding)
+                let encoding = String.Encoding(rawValue: nsEncoding)
+
+                return .success(mime, encoding)
+            } catch let err as NSError {
+                return .failure("cannot parse header: \(err)")
+            }
+        case "3":
+            do {
+                let regex = try NSRegularExpression(pattern: #"^\d+\s+(\S.*)$"#, options: [])
+                let range = NSRange(header.startIndex..<header.endIndex, in: header)
+                guard let m = regex.firstMatch(in: header, options: [], range: range),
+                    let range1 = Range(m.range(at: 1), in: header) else {
+                        return .failure("cannot parse header")
+                }
+                let to = String(header[range1])
+                return .redirect(to)
+            } catch let err as NSError {
+                return .failure("cannot parse header: \(err)")
+            }
         case "4", "5": return .failure("failure!")
         case "6": return .failure("client certificate required!")
         default: return .failure("unknown response code!")
@@ -166,8 +237,21 @@ extension GeminiClient: StreamDelegate {
 
             var pageTitle: String?
             var body = ""
+            var pre = false
             for line in content.components(separatedBy: "\n") {
                 let range = NSRange(line.startIndex..<line.endIndex, in: line)
+                if line == "```" {
+                    pre = !pre
+                    if pre {
+                        body.append("<pre><code>\n")
+                    } else {
+                        body.append("</code></pre>\n")
+                    }
+                    continue
+                }
+                if pre {
+                    body.append("\(line)\n")
+                }
                 if let m = h1Regex.firstMatch(in: line, options: [], range: range),
                     let range = Range(m.range(at: 1), in: line) {
                     let title = line[range]
@@ -197,10 +281,8 @@ extension GeminiClient: StreamDelegate {
                     body.append("<p>\(line)</p>\n")
                 }
             }
-            if pageTitle == nil {
-                pageTitle = self.url.absoluteDisplayString
-            }
-            return "<!DOCTYPE html><html lang=\"en\"><head><title>\(pageTitle!)</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head><body>\n\(body)</body></html>"
+            let title = pageTitle ?? self.url.absoluteDisplayString
+            return "<!DOCTYPE html><html lang=\"en\"><head><title>\(title)</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head><body>\n\(body)</body></html>"
         } catch let err as NSError {
             return "Error: \(err)"
         }
